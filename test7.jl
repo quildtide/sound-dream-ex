b09c24c0265badad57c287a7dd080b00d9e81dee
quildtide <42811940+quildtide@users.noreply.github.com>
2022-12-15 02:10:21 -0500

    Remove initial Meanpool

--- test7.jl
+++ test7.jl
@@ -12,7 +12,7 @@ import Zygote
 const GENRE_TYPE = Int8
 const BASE_BATCH_SIZE = 2646000
 const CHANNEL_COUNT = 2
-const DATA_FOL = "data/aesthetics"
+const DATA_FOL = "../dreamsinger/data/aesthetics"
 const GENRE_COUNT = 3
 
 function get_metadata()
@@ -71,20 +71,19 @@ end
 MODEL = Chain(
             Conv((15,1), CHANNEL_COUNT => 16, identity, pad = SamePad()),
             Dropout(.25),
+            swish,
+            Conv((15,1), 16 => 32, identity, dilation = (9, 1), pad = SamePad()),
+            Dropout(.35),
             MeanPool((32, 1)),
             swish,
-            Conv((15,1), 16 => 64, identity, dilation = (9, 1), pad = SamePad()),
-            Dropout(.35),
+            Conv((9,1), 32 => 64, identity, dilation = (7, 1), pad = SamePad()),
+            Dropout(.45),
             MeanPool((8, 1)),
             swish,
-            Conv((9,1), 64 => 128, identity, dilation = (7, 1), pad = SamePad()),
-            Dropout(.45),
-            MeanPool((4, 1)),
-            swish,
-            Conv((1,1), 128 => GENRE_COUNT, identity, dilation = (5, 1)),
+            Conv((1,1), 64 => GENRE_COUNT, identity, dilation = (5, 1)),
             Dropout(.5),
             AdaptiveMeanPool((1, 1)), # guaranteed dims: (256, 1, 128, B)
-            flatten,
+            Flux.flatten,
             logsoftmax
         ) |> gpu
 
