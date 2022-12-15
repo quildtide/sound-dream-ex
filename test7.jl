efb68760a3c3e83511d06a4f9f217ac86d27d24c
quildtide <42811940+quildtide@users.noreply.github.com>
2022-12-15 01:15:18 -0500

    Remove dilation, change data folder

--- test7.jl
+++ test7.jl
@@ -12,7 +12,7 @@ import Zygote
 const GENRE_TYPE = Int8
 const BASE_BATCH_SIZE = 2646000
 const CHANNEL_COUNT = 2
-const DATA_FOL = "data/aesthetics"
+const DATA_FOL = "../dreamsinger/data/aesthetics"
 const GENRE_COUNT = 3
 
 function get_metadata()
@@ -69,22 +69,22 @@ function dataloader(select_dict, chan, lengths, genres)
 end
 
 MODEL = Chain(
-            Conv((15,1), CHANNEL_COUNT => 16, identity, dilation = (9, 1), pad = SamePad()),
+            Conv((15,1), CHANNEL_COUNT => 16, identity, pad = SamePad()),
             Dropout(.25),
             MeanPool((32, 1)),
             swish,
-            Conv((15,1), 16 => 64, identity, dilation = (9, 1), pad = SamePad()),
+            Conv((15,1), 16 => 64, identity, pad = SamePad()),
             Dropout(.35),
             MeanPool((8, 1)),
             swish,
-            Conv((9,1), 64 => 128, identity, dilation = (7, 1), pad = SamePad()),
+            Conv((9,1), 64 => 128, identity, pad = SamePad()),
             Dropout(.45),
             MeanPool((4, 1)),
             swish,
-            Conv((1,1), 128 => GENRE_COUNT, identity, dilation = (5, 1)),
+            Conv((1,1), 128 => GENRE_COUNT, identity),
             Dropout(.5),
             AdaptiveMeanPool((1, 1)), # guaranteed dims: (256, 1, 128, B)
-            flatten,
+            Flux.flatten,
             logsoftmax
         ) |> gpu
 
